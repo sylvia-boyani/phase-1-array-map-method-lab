e8ad4aa8d2e1c9c35ec4c619bc9c0125714a3f7e
sylvia boyani <sylvia.boyani@student.moringaschool.com>
2022-06-13 07:37:57 +0300

    commit tree b15a5535213ac8c174584fabfb00c224260e5662

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
-const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
+const tutorials =[
+"what does the this keyword mean?",
+"What is the Constructor OO pattern?",
+"implementing Blockchain Web API",
+"The Test Driven Development Workflow",
+"What is NaN and how Can we Check for it",
+"What is the difference between stopPropagation and preventDefault?",
+"Immutable State and Pure Functions",
+"what is the difference between == and ===?",
+"what is the difference between event capturing and bubbling?",
+"what is JSONP?"
 ];
 
 const titleCased = () => {
-  return tutorials
-}
+return tutorials
+
+return tutorials.map((title) =>
+  title
+
+    .split(" ")
+    
+    .map((word) => word.slice(0, 1).toLocaleUpperCase() + word.slice(1))
+  
+    .join(" ")
+);
+};
